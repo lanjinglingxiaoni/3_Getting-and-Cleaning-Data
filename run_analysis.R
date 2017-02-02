82b24583c83ee014f091bc45b01e1b874e3a0155
Sophie <lanjinglingxiaoni@hotmail.com>
2017-02-02 16:01:48 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -40,4 +40,4 @@ colnames(data.mean_std)<- gsub("(t|f)(Body|Gravity)(Body|*)", "\\1.\\2.", colnam
 ## 5. From the data set in step 4, creates a second, independent tidy data set with the average 
 ###    of each variable for each activity and each subject.
 tidy <- ddply(data.mean_std, .(ID, Activity), numcolwise(mean))
-write.csv(tidy, file="./data/tidy.csv", quote = FALSE)
+write.table(tidy, file="./data/tidy.txt", row.names = FALSE)
